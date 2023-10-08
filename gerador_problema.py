152f632e40f2b4b9e3e2be7bcfae0b52d515a9a6
Joao-Moura <jaummoura15@gmail.com>
2023-10-08 12:35:47 -0300

    Convertendo gerador para iniciar em 0 em vez de 1

--- gerador_problema.py
+++ gerador_problema.py
@@ -1,46 +1,44 @@
 def main():
     entradas = []
-    linha = coluna = 1
+    linha = coluna = 0
 
     while True:
         try:
             valores = input()
             entradas += [
-                f"(lampada-em x{linha} y{coluna})" +
-                (f" (ta-quebrada x{linha} y{coluna})" if valor.islower() else "") +
-                (f" (ta-ligada x{linha} y{coluna})" if valor.lower() == 'l' else "")
-                for coluna, valor in enumerate(valores, 1)
+                ' '.join([
+                    (f"(ta-quebrada x{linha} y{coluna})" if valor.islower() else ""),
+                    (f"(ta-ligada x{linha} y{coluna})" if valor.lower() == 'l' else "")
+                ]).strip()
+                for coluna, valor in enumerate(valores)
+                if valor in ['L', 'l', 'd']
             ]
 
             linha += 1
-            coluna = len(valores)
+            coluna = len(valores) - 1
         except (EOFError, KeyboardInterrupt):
             break
 
     incrementos = []
-    for l in range(1, linha-1):
+    for l in range(0, linha-1):
         incrementos.append(f'(inc x{l} x{l+1})')
-        incrementos.append(f'(dec x{l+1} x{l})')
+        incrementos.append(f'(inc x{l+1} x{l})')
 
-    for c in range(1, coluna):
+    for c in range(0, coluna):
         incrementos.append(f'(inc y{c} y{c+1})')
-        incrementos.append(f'(dec y{c+1} y{c})')
+        incrementos.append(f'(inc y{c+1} y{c})')
 
     init = '\n'.join(entradas + incrementos)
-    objects = ' '.join([f'x{l}' for l in range(1, linha)] + [f'y{c}' for c in range(1, coluna+1)]) + ' - posicao'
+    objects = ' '.join([f'x{l}' for l in range(0, linha)]) + ' - linha ' + ' '.join([f'y{c}' for c in range(0, coluna+1)]) + ' - coluna'
 
     goals = []
-    for l in range(1, linha):
-        for c in range(1, coluna+1):
+    for l in range(0, linha):
+        for c in range(0, coluna+1):
             goals.append(f'(not (ta-ligada x{l} y{c}))')
     goals = '\n'.join(goals)
 
-    # print(init)
-    # print(objects)
-    # print(goals)
-
     with open('problema.pddl', 'w') as f:
-        f.write(fr"""
+        f.write(f"""
             (define (problem lightsoutproblem)
                 (:domain lightsout)
                 (:objects {objects})
